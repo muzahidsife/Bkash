bafb714c8f6cdb39734059d1761fd0fd1ac050e2
Muzahid <muzahidsife@gmail.com>
2023-06-29 23:03:29 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+#incomplete code
+#incomplete code
+#incomplete code
+
+def all():
+  print("1. Send Money")
+  print("2. Send Money to Non-Bkash User")
+  print("3. Mobile Recharge")
+  print("4. Payment")
+  print("5. Cash Out")                                   #incomplete code
+  print("6. Pay Bill")
+  print("7. Microfinance")
+  print("8. Download Bkash App")
+  print("9. My Bkash")
+  print("10. Reset PIN")
+#incomplete code
+#incomplete code
+#incomplete code
+def login():
+
+  loginn = input("Bkash dial login code : ")
+  if loginn == "*247#":
+    all()
+  else:
+    print("Invalid login code")
+  a = input()
+  if a == "1":
+
+    sendmoney = input("Enter receiver account no: ")
+    if sendmoney == sendmoney:
+       amount = input("Enter amount : ")
+       if amount == amount:
+         pin = input("Enter your pin number : ")
+         if pin == pin:
+          print(f"{amount} tk send to {sendmoney} this number")
+  elif a == "2":
+    print("x")
+if __name__ == "__main__":
+    login()
+
+#incomplete code
+#incomplete code
+#incomplete code
+#incomplete code

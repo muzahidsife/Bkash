260fadacd15ef440ebfc20ad9fbee53b376ad12c
Muzahid <muzahidsife@gmail.com>
2023-06-30 18:37:05 +0600

    Added  Send Money to Non-Bkash User,  Mobile Recharge,Payment,Cash Out,Pay Bill,Microfinance,   My Bkash

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 #incomplete code
 #incomplete code
 #incomplete code
+def my_bkash():
+    print("1. Check balance")
+    print("2. request statement")
+    print("3. Change PIN")
+    print("4. Priyo Number")
+    print("5. Save Bill")  # incomplete code
+    print("6. Update MNP")
+    print("7. Confirm iPhone login")
+    print("8. iPhone PIN reset")
+    print("9. Helpline")
+    print("0 Main menu")
 
 def all():
   print("1. Send Money")
@@ -20,7 +31,7 @@ def login():
 
   loginn = input("Bkash dial login code : ")
   if loginn == "*247#":
-    all()
+   all()
   else:
     print("Invalid login code")
   a = input()
@@ -33,8 +44,98 @@ def login():
          pin = input("Enter your pin number : ")
          if pin == pin:
           print(f"{amount} tk send to {sendmoney} this number")
+    back =input()
+    if back == "0":
+     return all()
   elif a == "2":
-    print("x")
+      sendmoney = input("Enter receiver account no: ")
+      if sendmoney == sendmoney:
+          amount = input("Enter amount : ")
+          if amount == amount:
+              pin = input("Enter your pin number : ")
+              if pin == pin:
+                  print(f"{amount} tk send to {sendmoney} this number")
+      back = input()
+      if back == "0":
+          return all()
+  elif a == "3":
+      sendmoney = input("Enter receiver account no: ")
+      if sendmoney == sendmoney:
+          amount = input("Enter amount : ")
+          if amount == amount:
+              pin = input("Enter your pin number : ")
+              if pin == pin:
+                  print(f"{amount} tk recharge to {sendmoney} this number")
+      back = input()
+      if back == "0":
+          return all()
+  elif a == "4":
+      sendmoney = input("Enter receiver account no: ")
+      if sendmoney == sendmoney:
+          amount = input("Enter amount : ")
+          if amount == amount:
+              pin = input("Enter your pin number : ")
+              if pin == pin:
+                  print(f"{amount} tk paid to {sendmoney} this number")
+      back = input()
+      if back == "0":
+          return all()
+  elif a == "5":
+      sendmoney = input("Enter agent account no: ")
+      if sendmoney == sendmoney:
+          amount = input("Enter amount : ")
+          if amount == amount:
+              pin = input("Enter your pin number : ")
+              if pin == pin:
+                  print(f"{amount} tk cashout from {sendmoney} this number")
+      back = input()
+      if back == "0":
+          return all()
+  elif a == "6":
+      sendmoney = input("Enter receiver account no: ")
+      if sendmoney == sendmoney:
+          amount = input("Enter amount : ")
+          if amount == amount:
+              pin = input("Enter your pin number : ")
+              if pin == pin:
+                  print(f"{amount} tk recharge to {sendmoney} this number")
+      back = input()
+      if back == "0":
+          return all()
+  elif a == "7":
+      sendmoney = input("Enter receiver account no: ")
+      if sendmoney == sendmoney:
+          amount = input("Enter amount : ")
+          if amount == amount:
+              pin = input("Enter your pin number : ")
+              if pin == pin:
+                  print(f"{amount} tk bill paid to {sendmoney} this number")
+      back = input()
+      if back == "0":
+          return all()
+  elif a == "8":
+      print("session timeout, press 0")
+      back = input()
+      if back == "0":
+          return all()
+  elif a == "9":
+      my_bkash()
+      statement = input()
+      if statement == "1":
+         print("Bhai eikhaneo tk khujen?")
+      back = input()
+      if back == "0":
+          return my_bkash()
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     login()
 
